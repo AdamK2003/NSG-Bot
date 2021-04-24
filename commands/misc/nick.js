6158e2894e1030849e2f4ce9650de1089a26e7ca
Max <78378142+Space-Rocket-Discord@users.noreply.github.com>
2021-04-24 22:11:37 +0200

    Update nick.js

--- commands/misc/nick.js
+++ commands/misc/nick.js
@@ -1,13 +1,19 @@
 module.exports.run =(client, message, args) => {
-  message.channel.send("a");
-};
+   const user = message.mentions.users.first();
+      const target = message.guild.member(user);
+      let nick = args.slice(0).join(' ');
 
-module.exports.help = {
-   name: "nick",
-   aliases: ['nick'],
-   category: 'misc',
-   description: "Nick yourself or someone",
-   usage: "(wyw)",
-   cooldown: 10,
-   args: true
-};     
+      target.setNickname(nick).then(async m => {
+         await message.channel.send(`${target} nickname changed to "${nick}"`)
+      })
+   };
+ 
+ module.exports.help = {
+    name: "nick",
+    aliases: ['nick'],
+    category: 'misc',
+    description: "Nick yourself or someone",
+    usage: "(wyw)",
+    cooldown: 10,
+    args: true
+ };     
