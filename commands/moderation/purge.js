bb7ab8565e38711242f72a2566e08e2ee8df9404
Max <78378142+Space-Rocket-Discord@users.noreply.github.com>
2021-04-29 19:39:25 +0200

    Update purge.js

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,19 +1,12 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-   if (isNaN(args[0]) || (args[0] < 1 || args[0] > 100)) return message.channel.send(`Please ${message.author},specify a number beteen 1 and 150!`);
+   if (isNaN(args[0]) || (args[0] < 1 || args[0] > 100)) return message.channel.send(`Please ${message.author},specify a number beteen 1 and 100!`);
 
    const messages = await message.channel.messages.fetch({
       limit: Math.min(args[0], 100),
       before: message.id,
    });
-
-    // If the author don't have perm
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!');
-    // If the bot don't have perm
-    if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.');
-    // If the bot don't find the user
-    if(!target) return message.channel.send('Can\'t seem to find this user.');
 
    const embed = new MessageEmbed()
        .setColor("#f20000")
