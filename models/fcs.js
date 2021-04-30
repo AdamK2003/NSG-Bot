cb81673ff323d05ada02699de8c0995238dfcf29
Max <78378142+Space-Rocket-Discord@users.noreply.github.com>
2021-04-30 21:13:19 +0200

    Update fcs.js

--- models/fcs.js
+++ models/fcs.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose')
 
+const fcsSchema = new friendcode({
+       _id: mongoose.Types.ObjectId(),
+       username: messager.author.tag,
+       userID: message.author.id,
+       fc: fc,
+       guildID: message.guild.id,
+       time: message.createdAt.toUTCString()
+   });
+
+module.exports = mongoose.model("Fc", reportSchema);
