b4ba6e2d23bb8a252444dd7d57e9f81dcc899ca7
AdamSkI2003 <60717729+AdamK2003@users.noreply.github.com>
2021-04-30 17:57:14 +0200

    fix purge perms (and some indentation)

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-   if (isNaN(args[0]) || (args[0] < 1 || args[0] > 100)) return message.channel.send(`Please ${message.author},specify a number beteen 1 and 150!`);
+   if (isNaN(args[0]) || (args[0] < 1 || args[0] > 100)) return message.channel.send(`${message.author} Please specify a number beteen 1 and 100!`);
 
    const messages = await message.channel.messages.fetch({
       limit: Math.min(args[0], 100),
@@ -9,17 +9,16 @@ module.exports.run = async (client, message, args) => {
    });
 
     // If the author don't have perm
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!');
+   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('You can\'t use that!');
     // If the bot don't have perm
-    if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.');
-    // If the bot don't find the user
-    if(!target) return message.channel.send('Can\'t seem to find this user.');
+   if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send('I don\'t have the right permissions.');
+   
 
    const embed = new MessageEmbed()
-       .setColor("#f20000")
-       .setAuthor(`By ${message.author.tag}`, message.author.displayAvatarURL())
-       .addField(`Purge`, `${message.author.tag} deleted ${args[0]} messages in ${message.channel}`)
-       .setTimestamp()
+      .setColor("#f20000")
+      .setAuthor(`By ${message.author.tag}`, message.author.displayAvatarURL())
+      .addField(`Purge`, `${message.author.tag} deleted ${args[0]} messages in ${message.channel}`)
+      .setTimestamp()
 
          await message.channel.bulkDelete(messages).then(async m => {
             await message.channel.send(embed);
@@ -33,7 +32,7 @@ module.exports.help = {
    name: "purge",
    aliases: ['purge'],
    category: 'moderation',
-   description: "Delete a certain amunt of message between 1 and 100",
+   description: "Delete a certain amount of messages (between 1 and 100 at a time)",
    usage: "(amount)",
    cooldown: 0,
    args: true
