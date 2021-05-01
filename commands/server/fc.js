a673b465ab398c95909218e596436105096939cc
AdamSkI2003 <60717729+AdamK2003@users.noreply.github.com>
2021-05-01 17:50:11 +0200

    fix

--- commands/server/fc.js
+++ commands/server/fc.js
@@ -23,7 +23,9 @@ module.exports.help = {
 
 module.exports.run = async (client, message, args) => {
     let rUser = message.mentions.members.first();
-    if(!rUser) let rUser = message.member
+    if(!rUser) {
+        let rUser = message.member
+    }
     FC.find({userID: rUser.id}, (err, arr) => {
         if(!arr) return message.channel.send(`${rUser.tag} doesn't have a friend code set!`)
         message.channel.send(`Friend code of ${rUser}: ` + arr[0].fc);
