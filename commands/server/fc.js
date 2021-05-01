b15aab4be27e9454aedbd08ca68db7034b71243e
Max <78378142+Space-Rocket-Discord@users.noreply.github.com>
2021-05-01 17:42:00 +0200

    Update fc.js

--- commands/server/fc.js
+++ commands/server/fc.js
@@ -1,3 +1,4 @@
+  
 const mongoose = require('mongoose')
 
 const db = process.env.DB
@@ -23,12 +24,10 @@ module.exports.help = {
 module.exports.run = async (client, message, args) => {
     let rUser = message.mentions.members.first();
     if(!rUser) return message.channel.send("Invalid user!")
-    try {
-        FC.find({userID: rUser.id}, (err, arr) => {
-            message.channel.send(`Friend code of ${rUser} ` + arr[0].fc);
-        })
-    } catch(err) {
-        message.channel.send(`An error ocurred,this might be because ${rUser.tag} don't have a fc set!`);
-    }
+    if(!arr) return message.channel.send(`An error ocurred,this might be because ${rUser.tag} don't have a fc set!`);
+FC.find({userID: rUser.id}, (err, arr) => {
+
+message.channel.send(`Friend code of ${rUser} ` + arr[0].fc)
+})
 return
 }
