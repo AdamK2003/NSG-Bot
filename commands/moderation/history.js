5b9b6ecb61a162a06e079ffb612ae3c62e533230
Max <78378142+Space-Rocket-Discord@users.noreply.github.com>
2021-05-02 21:16:29 +0200

    Update history.js

--- commands/moderation/history.js
+++ commands/moderation/history.js
@@ -39,7 +39,7 @@ Bans.find({ userID: rUser.id, guildID: message.guild.id }, (err, arr) => {
       var i;
       for(i = 0; i < arr.length; i++) {
           embed.addField('\u200b', '\u200b')
-          embed.addField("**Bans**\nBanned by " + arr[i].BannedBy, "Reason: " + arr[i].reason)
+          embed.addField("**Bans**\nBanned by " + arr[i].BannedByID, "Reason: " + arr[i].reason)
           embed.addFields(
               { name: "Time", value: arr[i].time, inline: true },
               { name: "Ban ID", value: arr[i]._id, inline: true }
@@ -53,7 +53,7 @@ Report.find({ userID: rUser.id, guildID: message.guild.id }, (err, arr) => {
       var i;
       for(i = 0; i < arr.length; i++) {
           embed.addField('\u200b', '\u200b')
-          embed.addField("**Warns**\nWarned by " + arr[i].WarnedBy, "Reason: " + arr[i].reason)
+          embed.addField("**Warns**\nWarned by " + arr[i].WarnedByID, "Reason: " + arr[i].reason)
           embed.addFields(
               { name: "Time", value: arr[i].time, inline: true },
               { name: "Warn ID", value: arr[i]._id, inline: true }
@@ -67,7 +67,7 @@ Mutes.find({ userID: rUser.id, guildID: message.guild.id }, (err, arr) => {
       var i;
       for(i = 0; i < arr.length; i++) {
           embed.addField('\u200b', '\u200b')
-          embed.addField("**Mutes**\nMuted by " + arr[i].BannedBy, "Reason: " + arr[i].reason)
+          embed.addField("**Mutes**\nMuted by " + arr[i].MutedByID, "Reason: " + arr[i].reason)
           embed.addFields(
               { name: "Time", value: arr[i].time, inline: true },
               { name: "Mute ID", value: arr[i]._id, inline: true }
