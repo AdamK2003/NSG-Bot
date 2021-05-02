7ad7a51c40724f294a87394160b83088826b37dc
Max <78378142+Space-Rocket-Discord@users.noreply.github.com>
2021-05-02 17:49:42 +0200

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,37 +1,64 @@
+const mongoose = require('mongoose')
+
+const db = process.env.DB
+
+
+mongoose.connect(db)
+
+const Ban = require(process.env.ROOTDIR + '/models/bans.js')
+
+
+const Discord = require('discord.js')
+
 const { MessageEmbed } = require("discord.js");
 
 module.exports.run =(client, message, args) => {
-   
    const user = message.mentions.users.first();
       const target = message.guild.member(user);
 
-         let reason = args.slice(1).join(' ');
-         if(!reason) reason = "Not Specified";
+      let reason = args.slice(1).join(' ');
+      if(!reason) reason = "None";
 
-       if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!');
-       if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.');
+      let role = message.guild.roles.cache.find(r => r.name === "muted");
+
+       if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('You can\'t use that!');
+       if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send('I don\'t have the right permissions.');
        if(!target) return message.channel.send('Can\'t seem to find this user.');
-       if(!reason) return(reason = "No reason were specified");
 
        const embed = new MessageEmbed()
-       .setColor("#f20000")
-       .setAuthor(`By ${message.author.tag}`, message.author.displayAvatarURL())
-       .addField(`Ban!`, `${message.author.tag} banned ${user.tag} from the server!`)
+       .setColor("RANDOM")
+       .setAuthor(`By ${message.author}`, message.author.displayAvatarURL())
+       .addField(`Ban!`, `${message.author} banned ${user} from the server`)
        .addField(`Reason:`, `${reason}`)
        .setTimestamp()
-
+       
          message.channel.send(embed).then(async m => {
             await client.channels.cache.get("823962434703327253").send(embed);
             target.ban();
          })
+         
+         const ban = new Ban({
+            _id: mongoose.Types.ObjectId(),
+            username: target.user.tag,
+            userID: target.id,
+            reason: reason,
+            BannedBy: message.author.tag,
+            BannedByID: message.author.id,
+            guildID: message.guild.id,
+            time: message.createdAt.toUTCString() 
+        })
+        
+        ban.save()
+     .then(result => console.log(result))
+     .catch(err => console.log(err));
 };
 
 module.exports.help = {
    name: "ban",
    aliases: ['ban'],
    category: 'moderation',
-   description: "ban a user",
+   description: "Ban a user",
    usage: "(member) (reason)",
    cooldown: 0,
    args: true
-};
+};     
